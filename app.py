e6bbd1d2eacd0f3c9e9502cd314fcb3841df7023
Ganesh-73005 <163154009+Ganesh-73005@users.noreply.github.com>
2025-04-15 00:01:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def checkout():
         return f"Error uploading invoice: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
